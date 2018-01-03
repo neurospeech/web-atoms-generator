7da38b414d04639e92b8a4ca5b01833cb8e2c2cc
Akash Kava <support@neurospeech.com>
2018-01-03 15:53:16 +0530

    wip

--- bin/html-component.js
+++ bin/html-component.js
@@ -113,7 +113,7 @@ class HtmlComponent {
 							});`);
                         continue;
                     }
-                    if (/^atom\-/i.test(key)) {
+                    if (/^atom\-/i.test(key) && key !== "atom-type" && key !== "atom-template" && key !== "atom-presenter") {
                         inits.push(`this.setLocalValue('${ckey}',${JSON.stringify(v)})`);
                         continue;
                     }
